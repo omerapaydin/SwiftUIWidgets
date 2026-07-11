10f05c2eef8806bf9a5af53f18fef83a1ceca7e9
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-12 00:43:53 +0300

    Initial Commit

---
+++ SwiftUIWidgets/SwiftUIWidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWidgetsApp.swift
+//  SwiftUIWidgets
+//
+//  Created by Ömer Apaydın on 12.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
