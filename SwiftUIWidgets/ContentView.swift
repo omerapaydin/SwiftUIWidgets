61426dba04f88df1ccef695e2669469b9a7193cb
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-12 15:58:12 +0300

    First

--- SwiftUIWidgets/ContentView.swift
+++ SwiftUIWidgets/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alinanVeri = ""
+    @State private var tf = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Gelen veri : \(alinanVeri)")
+            
+            TextField("Veri Giriniz", text: $tf)
+                .textFieldStyle(.bordered)
+                .padding()
+            
+            Button("Veriyi Al")
         }
-        .padding()
+       
     }
 }
 
