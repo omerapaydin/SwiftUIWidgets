c7a0bfd859e056d3af29bb0bc8181142aae1d70a
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-12 16:31:10 +0300

    ImageView edit

---
+++ SwiftUIWidgets/ImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageView.swift
+//  SwiftUIWidgets
+//
+//  Created by Ömer Apaydın on 12.07.2026.
+//
+
+import SwiftUI
+
+struct ImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageView()
+}
