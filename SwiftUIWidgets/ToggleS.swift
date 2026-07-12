0d7497a740c48d362edf5f6c79f83d529b4d072a
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-12 16:46:13 +0300

    Toggle

---
+++ SwiftUIWidgets/ToggleS.swift
@@ -0,0 +1,18 @@
+//
+//  ToggleS.swift
+//  SwiftUIWidgets
+//
+//  Created by Ömer Apaydın on 12.07.2026.
+//
+
+import SwiftUI
+
+struct ToggleS: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToggleS()
+}
