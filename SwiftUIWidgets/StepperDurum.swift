58fdc4f45a31c02fa4074909623336e862505652
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-12 17:06:27 +0300

    StepperDurum

---
+++ SwiftUIWidgets/StepperDurum.swift
@@ -0,0 +1,18 @@
+//
+//  StepperDurum.swift
+//  SwiftUIWidgets
+//
+//  Created by Ömer Apaydın on 12.07.2026.
+//
+
+import SwiftUI
+
+struct StepperDurum: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StepperDurum()
+}
