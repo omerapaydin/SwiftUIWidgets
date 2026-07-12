ef6fa35f61a259b01d6345bec6c8a61651e23d6d
Ömer Apaydın <omerapaydin90@gmail.com>
2026-07-12 17:06:35 +0300

    StepperDurum devam

--- SwiftUIWidgets/StepperDurum.swift
+++ SwiftUIWidgets/StepperDurum.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct StepperDurum: View {
+    
+    @State private var stepperDurum = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Stepper("Stepper",value: $stepperDurum,in: 0...10)
+                .padding()
+            
+            Text("Sonuç: \(stepperDurum)")
+            
+            Button("GÖSTER"){
+                print("Stepper Durum : \(stepperDurum)")
+            }
+            
+        }
     }
 }
 
